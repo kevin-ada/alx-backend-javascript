eb24812bee25abac6db504bec2eb988e2634fba8
kim-thuita <kevinkoech265@gmail.com>
2023-11-30 04:00:45 +0300

    "if task 1 works"

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,6 +1,6 @@
 export default function getFullResponseFromAPI(success) {
   return Promise((resolve, reject) => {
-    if (success) {
+    if (success === true) {
       resolve({ status: 200, body: 'Success' });
     } else {
       reject('The fake API is not working currently');
