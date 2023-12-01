04cf0d82f8d0dc4f12f5ad8fba4958d1de31abfa
kim-thuita <kevinkoech265@gmail.com>
2023-12-01 10:13:45 +0300

    "Filename error"

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,6 +1,5 @@
 export default function uploadPhoto(filename) {
-  // eslint-disable-next-line new-cap
-  return new Promise.reject(
-    new Error(`${filename} cannot be processed`),
-  );
+  return new Promise((resolve, reject) => {
+    reject(new Error(`${filename} cannot be processed`));
+  });
 }
