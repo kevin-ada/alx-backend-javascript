02ee0f8207ed3ed37647bd3370f825a213b87375
kim-thuita <kevinkoech265@gmail.com>
2023-12-04 17:07:10 +0300

    "try - catch"

---
+++ 0x01-ES6_promise/8-try.js
@@ -0,0 +1,11 @@
+// eslint-disable-next-line consistent-return
+export default function divideFunction(numerator, denominator) {
+  try {
+    if (denominator === 0) {
+      return numerator / denominator;
+    }
+    throw new Error('cannot divide by 0');
+  } catch (e) {
+    console.log(e.message);
+  }
+}
