30156b061c947c929537b51b13fb5fcb98dd5995
kim-thuita <kevinkoech265@gmail.com>
2023-12-04 17:18:26 +0300

    "try - catch"

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,12 +1,8 @@
 export default function divideFunction(numerator, denominator) {
-  try {
-    if (denominator === 0) {
-      throw new Error('cannot divide by 0');
-    }
-    return numerator / denominator;
-  } catch (e) {
-    return e;
+  if (denominator === 0) {
+    throw new Error('Cannot divide by 0');
   }
+  return numerator / denominator;
 }
 
 console.log(divideFunction(10, 2));
