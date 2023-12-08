572fb86d58eeb98d41d53f628884586bf4dbe8ae
kim-thuita <kevinkoech265@gmail.com>
2023-12-08 22:10:38 +0300

    "mapping changes"

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -2,9 +2,5 @@ export default function getListStudentIds(getListStudents) {
   if (typeof getListStudents !== 'function') {
     return [];
   }
-  const listStudents = getListStudents();
-  if (!Array.isArray(listStudents)) {
-    return [];
-  }
-  return listStudents.map((student) => student.id);
+  return getListStudents.map((student) => student.id);
 }
